0cf04039ad03a44f35754b6a95e6855d939aa289
Nurali <nurali.kalliev@gmail.com>
2025-03-20 21:08:07 +0500

    Home page

---
+++ frontend/Homepage.tsx
@@ -0,0 +1,606 @@
+import * as React from "react";
+import {Text, StyleSheet, View, Image} from "react-native";
+import Rectangle23 from "../assets/rectangle-23.svg"
+import Fluentemojiflatfire from "../assets/fluentemojiflatfire.svg"
+import Rectangle229 from "../assets/rectangle-229.svg"
+import Rectangle232 from "../assets/rectangle-232.svg"
+import Rectangle234 from "../assets/rectangle-234.svg"
+import Group88 from "../assets/group-88.svg"
+import Group from "../assets/group.svg"
+import Group2 from "../assets/group-2.svg"
+import Vuesaxlinearheart from "../assets/vuesaxlinearheart.svg"
+
+const IPhone1314 = () => {
+  	
+  	return (
+    		<View style={styles.iphone131417}>
+      			<View style={styles.iphone16Pro45}>
+        				<Text style={styles.welcomeAgzam}>Welcome, Agzam!</Text>
+        				<View style={[styles.iphone16Pro45Child, styles.iphone16ShadowBox]} />
+        				<View style={[styles.iphone16Pro45Item, styles.rectangleViewPosition]} />
+        				<Image style={styles.iphone16Pro45Inner} resizeMode="cover" source="Group 1.png" />
+        				<Rectangle23 style={[styles.rectangleIcon, styles.rectangleIconLayout]} width={106} height={31} />
+        				<Text style={styles.continue}>Continue</Text>
+        				<Text style={[styles.mathIntermediate, styles.mathIntermediateFlexBox]}>Math Intermediate</Text>
+        				<Text style={styles.challengeOfThe}>{`Challenge
+          					of the day`}</Text>
+        				<Text style={styles.text}>59%</Text>
+        				<View style={[styles.rectangleView, styles.iphone16ChildLayout3]} />
+        				<Fluentemojiflatfire style={[styles.fluentEmojiFlatfireIcon, styles.iphone16Pro45Child7Layout]} width={89} height={89} />
+        				<View style={styles.iphone16Pro45Child1} />
+        				<Text style={[styles.streak14Days, styles.keepGoingLayout]}>Streak: 14 days</Text>
+        				<Text style={[styles.keepGoing, styles.keepGoingLayout]}>Keep going!</Text>
+        				<Text style={[styles.rewardsAvailable, styles.mathIntermediateFlexBox]}>Rewards available</Text>
+        				<Text style={[styles.numericalNinja, styles.mathWizardTypo]}>Numerical Ninja</Text>
+        				<Text style={[styles.mathWizard, styles.mathWizardTypo]}>Math Wizard</Text>
+        				<Text style={[styles.text1, styles.textLayout]}>50</Text>
+        				<Text style={[styles.text2, styles.textLayout]}>150</Text>
+        				<Image style={[styles.image19Icon, styles.image19IconLayout]} resizeMode="cover" source="image 19.png" />
+        				<Image style={styles.image20Icon} resizeMode="cover" source="image 20.png" />
+        				<Image style={styles.image21Icon} resizeMode="cover" source="image 21.png" />
+        				<Image style={styles.image22Icon} resizeMode="cover" source="image 22.png" />
+        				<Rectangle229 style={[styles.iphone16Pro45Child2, styles.iphone16ChildLayout2]} width={163} height={139} />
+        				<View style={[styles.iphone16Pro45Child3, styles.iphone16ChildLayout1]} />
+        				<Text style={[styles.brainstorm, styles.brainstormLayout]}>Brainstorm</Text>
+        				<Text style={[styles.testYourLogic, styles.brainstorm1Position]}>Test your logic!</Text>
+        				<View style={[styles.iphone16Pro45Child4, styles.iphone16ChildLayout]} />
+        				<Text style={[styles.start, styles.startTypo]}>Start</Text>
+        				<Rectangle232 style={[styles.iphone16Pro45Child5, styles.iphone16ChildLayout2]} width={163} height={186} />
+        				<View style={[styles.iphone16Pro45Child6, styles.iphone16ChildBorder]} />
+        				<Text style={[styles.brainstorm1, styles.brainstorm1Position]}>Brainstorm</Text>
+        				<Text style={[styles.minutes, styles.brainstormLayout]}>5 minutes</Text>
+        				<Rectangle234 style={[styles.iphone16Pro45Child7, styles.iphone16Pro45Child7Layout]} width={89} height={31} />
+        				<Text style={[styles.start1, styles.startTypo]}>Start</Text>
+        				<View style={[styles.iphone16Pro45Child8, styles.iphone16ChildLayout]} />
+        				<Text style={[styles.more, styles.startTypo]}>More</Text>
+        				<View style={styles.frameView} />
+        				<Group88 style={styles.groupIcon} width={390} height={65} />
+        				<Group style={[styles.groupIcon1, styles.iconLayout]} />
+        				<View style={[styles.iphone16Pro45Child9, styles.tryPosition]} />
+        				<Text style={[styles.try, styles.tryPosition]}>Try!</Text>
+        				<Text style={[styles.testYourLogicRiddles, styles.testClr]}>{`Test your logic!Riddles, puzzles, paradoxes
+          					and more`}</Text>
+        				<Group2 style={[styles.iphone16Pro45Child10, styles.iphone16ChildLayout1]} width={47} height={48} />
+      			</View>
+      			<Vuesaxlinearheart style={[styles.vuesaxlinearheartIcon, styles.iconLayout]} />
+    		</View>);
+};
+
+const styles = StyleSheet.create({
+  	iphone16ShadowBox: {
+    		transform: [
+      			{
+        				rotate: "-0.1deg"
+      			}
+    		],
+    		borderWidth: 1,
+    		borderColor: "#debbff",
+    		borderStyle: "solid",
+    		elevation: 23,
+    		shadowRadius: 23,
+    		shadowOffset: {
+      			width: 0,
+      			height: 10
+    		},
+    		shadowColor: "rgba(0, 0, 0, 0.1)",
+    		borderRadius: 26,
+    		shadowOpacity: 1,
+    		position: "absolute"
+  	},
+  	rectangleViewPosition: {
+    		width: 380,
+    		left: "50%",
+    		marginLeft: -189.5
+  	},
+  	rectangleIconLayout: {
+    		height: 31,
+    		borderRadius: 17
+  	},
+  	mathIntermediateFlexBox: {
+    		alignItems: "center",
+    		display: "flex",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute"
+  	},
+  	iphone16ChildLayout3: {
+    		borderRadius: 15,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child7Layout: {
+    		width: 89,
+    		position: "absolute"
+  	},
+  	keepGoingLayout: {
+    		width: 291,
+    		left: 34,
+    		height: 77,
+    		lineHeight: 31,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute"
+  	},
+  	mathWizardTypo: {
+    		height: 17,
+    		fontSize: 18,
+    		left: 222,
+    		lineHeight: 15,
+    		color: "#fff",
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute"
+  	},
+  	textLayout: {
+    		width: 32,
+    		height: 17,
+    		lineHeight: 15,
+    		color: "#fff",
+    		fontSize: 15,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute"
+  	},
+  	image19IconLayout: {
+    		height: 47,
+    		position: "absolute"
+  	},
+  	iphone16ChildLayout2: {
+    		width: 163,
+    		left: 18
+  	},
+  	iphone16ChildLayout1: {
+    		height: 48,
+    		position: "absolute"
+  	},
+  	brainstormLayout: {
+    		width: 116,
+    		height: 26,
+    		color: "#272727",
+    		fontFamily: "DoHyeon-Regular",
+    		lineHeight: 38
+  	},
+  	brainstorm1Position: {
+    		left: 25,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		position: "absolute"
+  	},
+  	iphone16ChildLayout: {
+    		height: 35,
+    		width: 89,
+    		borderRadius: 17
+  	},
+  	startTypo: {
+    		textAlign: "center",
+    		fontSize: 15,
+    		color: "#000",
+    		fontFamily: "DoHyeon-Regular",
+    		lineHeight: 38
+  	},
+  	iphone16ChildBorder: {
+    		borderColor: "#ffdbc9",
+    		borderTopRightRadius: 15,
+    		borderTopLeftRadius: 15,
+    		borderWidth: 1,
+    		borderStyle: "solid",
+    		backgroundColor: "#fff"
+  	},
+  	iconLayout: {
+    		maxHeight: "100%",
+    		maxWidth: "100%",
+    		position: "absolute",
+    		overflow: "hidden"
+  	},
+  	tryPosition: {
+    		top: 340,
+    		position: "absolute"
+  	},
+  	testClr: {
+    		color: "#fff",
+    		lineHeight: 15
+  	},
+  	welcomeAgzam: {
+    		top: 53,
+    		left: 23,
+    		fontSize: 29,
+    		width: 281,
+    		height: 26,
+    		lineHeight: 38,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		color: "#272727",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child: {
+    		top: 106,
+    		width: 378,
+    		height: 123,
+    		left: "50%",
+    		marginLeft: -189.5,
+    		borderWidth: 1,
+    		borderColor: "#debbff",
+    		borderStyle: "solid",
+    		elevation: 23,
+    		shadowRadius: 23,
+    		shadowOffset: {
+      			width: 0,
+      			height: 10
+    		},
+    		shadowColor: "rgba(0, 0, 0, 0.1)",
+    		backgroundColor: "#fff"
+  	},
+  	iphone16Pro45Item: {
+    		top: 252,
+    		backgroundColor: "#5a6fe4",
+    		height: 136,
+    		transform: [
+      			{
+        				rotate: "-0.1deg"
+      			}
+    		],
+    		borderWidth: 1,
+    		borderColor: "#debbff",
+    		borderStyle: "solid",
+    		elevation: 23,
+    		shadowRadius: 23,
+    		shadowOffset: {
+      			width: 0,
+      			height: 10
+    		},
+    		shadowColor: "rgba(0, 0, 0, 0.1)",
+    		borderRadius: 26,
+    		shadowOpacity: 1,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Inner: {
+    		top: 111,
+    		left: 260,
+    		width: 110,
+    		height: 109,
+    		position: "absolute"
+  	},
+  	rectangleIcon: {
+    		top: 177,
+    		left: 50,
+    		position: "absolute"
+  	},
+  	continue: {
+    		top: 169,
+    		left: 68,
+    		width: 69,
+    		height: 46,
+    		justifyContent: "center",
+    		textAlign: "center",
+    		color: "#000",
+    		fontSize: 15,
+    		alignItems: "center",
+    		display: "flex",
+    		fontFamily: "DoHyeon-Regular",
+    		lineHeight: 38,
+    		position: "absolute"
+  	},
+  	mathIntermediate: {
+    		top: 124,
+    		left: 42,
+    		fontSize: 25,
+    		lineHeight: 28,
+    		width: 263,
+    		height: 50,
+    		textAlign: "left",
+    		alignItems: "center",
+    		display: "flex",
+    		fontFamily: "DoHyeon-Regular",
+    		color: "#272727"
+  	},
+  	challengeOfThe: {
+    		top: 259,
+    		left: 28,
+    		width: 191,
+    		height: 77,
+    		lineHeight: 31,
+    		color: "#fff",
+    		fontSize: 30,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute"
+  	},
+  	text: {
+    		top: 131,
+    		left: 289,
+    		width: 60,
+    		height: 72,
+    		fontSize: 30,
+    		justifyContent: "center",
+    		textAlign: "center",
+    		color: "#000",
+    		alignItems: "center",
+    		display: "flex",
+    		fontFamily: "DoHyeon-Regular",
+    		lineHeight: 38,
+    		position: "absolute"
+  	},
+  	rectangleView: {
+    		top: 410,
+    		backgroundColor: "#ffe8b7",
+    		height: 90,
+    		width: 380,
+    		left: "50%",
+    		marginLeft: -189.5
+  	},
+  	fluentEmojiFlatfireIcon: {
+    		top: 397,
+    		left: 295
+  	},
+  	iphone16Pro45Child1: {
+    		top: 552,
+    		left: 208,
+    		backgroundColor: "#339624",
+    		width: 175,
+    		height: 192,
+    		borderRadius: 26,
+    		position: "absolute"
+  	},
+  	streak14Days: {
+    		top: 406,
+    		fontSize: 35,
+    		color: "#000",
+    		width: 291
+  	},
+  	keepGoing: {
+    		top: 436,
+    		color: "#725d2f",
+    		fontSize: 20
+  	},
+  	rewardsAvailable: {
+    		top: 502,
+    		left: 198,
+    		color: "#010101",
+    		textAlign: "right",
+    		width: 176,
+    		height: 56,
+    		lineHeight: 15,
+    		fontSize: 20,
+    		alignItems: "center",
+    		display: "flex",
+    		fontFamily: "DoHyeon-Regular"
+  	},
+  	numericalNinja: {
+    		top: 600,
+    		width: 92
+  	},
+  	mathWizard: {
+    		top: 663,
+    		width: 99
+  	},
+  	text1: {
+    		top: 642,
+    		left: 244
+  	},
+  	text2: {
+    		left: 246,
+    		top: 572
+  	},
+  	image19Icon: {
+    		top: 573,
+    		left: 326,
+    		width: 45
+  	},
+  	image20Icon: {
+    		top: 568,
+    		left: 223,
+    		width: 19,
+    		height: 19,
+    		position: "absolute"
+  	},
+  	image21Icon: {
+    		top: 641,
+    		left: 224,
+    		width: 12,
+    		height: 18,
+    		position: "absolute"
+  	},
+  	image22Icon: {
+    		top: 637,
+    		left: 321,
+    		width: 50,
+    		height: 51,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child2: {
+    		top: 572,
+    		borderRadius: 15,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child3: {
+    		borderColor: "#ffdbc9",
+    		borderTopRightRadius: 15,
+    		borderTopLeftRadius: 15,
+    		borderWidth: 1,
+    		borderStyle: "solid",
+    		backgroundColor: "#fff",
+    		width: 163,
+    		left: 18,
+    		top: 572
+  	},
+  	brainstorm: {
+    		top: 584,
+    		fontSize: 17,
+    		left: 34,
+    		width: 116,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		position: "absolute"
+  	},
+  	testYourLogic: {
+    		top: 621,
+    		fontSize: 12,
+    		fontWeight: "600",
+    		fontFamily: "Roboto-Bold",
+    		width: 150,
+    		height: 37,
+    		lineHeight: 15,
+    		color: "#fff"
+  	},
+  	iphone16Pro45Child4: {
+    		top: 666,
+    		backgroundColor: "#fbbb9c",
+    		left: 32,
+    		position: "absolute"
+  	},
+  	start: {
+    		top: 664,
+    		left: 59,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child5: {
+    		top: 556,
+    		borderRadius: 15,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child6: {
+    		top: 551,
+    		left: 17,
+    		width: 164,
+    		height: 47,
+    		position: "absolute"
+  	},
+  	brainstorm1: {
+    		top: 517,
+    		width: 116,
+    		height: 26,
+    		color: "#272727",
+    		fontFamily: "DoHyeon-Regular",
+    		lineHeight: 38,
+    		fontSize: 20
+  	},
+  	minutes: {
+    		top: 563,
+    		fontSize: 20,
+    		left: 34,
+    		width: 116,
+    		alignItems: "center",
+    		display: "flex",
+    		textAlign: "left",
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child7: {
+    		top: 696,
+    		left: 32,
+    		borderRadius: 17
+  	},
+  	start1: {
+    		top: 692,
+    		left: 60,
+    		position: "absolute"
+  	},
+  	iphone16Pro45Child8: {
+    		top: 693,
+    		backgroundColor: "#b4fea9",
+    		left: 222,
+    		height: 35,
+    		position: "absolute"
+  	},
+  	more: {
+    		top: 691,
+    		left: 250,
+    		position: "absolute"
+  	},
+  	frameView: {
+    		top: 281,
+    		left: 284,
+    		width: 100,
+    		height: 100,
+    		position: "absolute",
+    		overflow: "hidden"
+  	},
+  	groupIcon: {
+    		top: 779,
+    		left: 0,
+    		position: "absolute"
+  	},
+  	groupIcon1: {
+    		height: "12.65%",
+    		width: "29%",
+    		top: "31.52%",
+    		right: "10.38%",
+    		bottom: "55.83%",
+    		left: "60.61%"
+  	},
+  	iphone16Pro45Child9: {
+    		backgroundColor: "#ffe168",
+    		height: 35,
+    		width: 89,
+    		borderRadius: 17,
+    		left: 32
+  	},
+  	try: {
+    		left: 64,
+    		textAlign: "center",
+    		fontSize: 15,
+    		color: "#000",
+    		fontFamily: "DoHyeon-Regular",
+    		lineHeight: 38
+  	},
+  	testYourLogicRiddles: {
+    		top: 633,
+    		width: 146,
+    		height: 27,
+    		left: 32,
+    		lineHeight: 15,
+    		fontSize: 20,
+    		alignItems: "center",
+    		display: "flex",
+    		fontFamily: "DoHyeon-Regular",
+    		position: "absolute",
+    		textAlign: "left"
+  	},
+  	iphone16Pro45Child10: {
+    		top: 786,
+    		left: 172
+  	},
+  	iphone16Pro45: {
+    		top: 0,
+    		shadowColor: "rgba(0, 0, 0, 0.25)",
+    		shadowOffset: {
+      			width: 0,
+      			height: 4
+    		},
+    		shadowRadius: 4,
+    		elevation: 4,
+    		width: 391,
+    		backgroundColor: "#fff",
+    		shadowOpacity: 1,
+    		left: 0,
+    		position: "absolute",
+    		overflow: "hidden",
+    		height: 844
+  	},
+  	vuesaxlinearheartIcon: {
+    		height: "5.21%",
+    		width: "11.54%",
+    		top: "93.48%",
+    		right: "64.1%",
+    		bottom: "1.3%",
+    		left: "24.36%"
+  	},
+  	iphone131417: {
+    		backgroundColor: "#f6f4f4",
+    		flex: 1,
+    		width: "100%",
+    		overflow: "hidden",
+    		height: 844
+  	}
+});
+
+export default IPhone1314;
