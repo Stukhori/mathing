cba4f983f9820c8fcba6f5e303f83e4510d595bd
KhDan007 <kh.dan007@gmail.com>
2025-03-23 21:44:47 +0500

    fix index file

--- frontend/app/_layout.js
+++ frontend/app/_layout.js
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="Intropage" options={{ title: "Welcome" }} />
+      <Stack.Screen name="index" options={{ title: "Welcome" }} />
       <Stack.Screen name="Signup" options={{ title: "Sign Up" }} />
       <Stack.Screen name="Signin" options={{ title: "Sign In" }} />
     </Stack>
